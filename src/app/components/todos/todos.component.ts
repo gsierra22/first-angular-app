0ada91258067462905dcc9909032616fe0582427
gsierra22 <giancarlosierra2011@hotmail.com>
2022-06-21 15:24:15 -0500

    completed angular todo list

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -10,6 +10,8 @@ export class TodosComponent implements OnInit {
 
   todos: Todo[]
 
+  inputTodo: string = ''
+
   constructor() { }
 
   ngOnInit(): void {
@@ -34,6 +36,14 @@ export class TodosComponent implements OnInit {
 
   deleteTodo(id:number){
     this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo(){
+    this.todos.push({
+      content:this.inputTodo,
+      completed:false
+    })
+    this.inputTodo=""
   }
 
 }
